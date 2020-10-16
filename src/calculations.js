00be8477bc93ee48d576b52cf15c74db2b09b655
Harish Gowda N <n.harishgowda@yahoo.com>
2020-10-16 14:15:48 +0530

    package-lock updated

--- src/calculations.js
+++ src/calculations.js
@@ -2,24 +2,16 @@ export default (distance, vehicle) => {
     var total_cost = 0;
     switch (vehicle) {
         case 'Piaggio Ape': {
-            if (distance > 0 && distance <= 10)
-                total_cost = distance * 125;
-            else if (distance > 10 && distance <= 20)
-                total_cost = distance * 120;
-            else if (distance > 20 && distance <= 30)
-                total_cost = distance * 110;
-            else if (distance > 30)
-                total_cost = distance * 100;
+            total_cost = 300
+            if(distance > 4){
+                total_cost += (distance - 4) * 10
+            }
         } break
         case 'Tata Ace': {
-            if (distance > 0 && distance <= 10)
-                total_cost = distance * 200;
-            else if (distance > 10 && distance <= 20)
-                total_cost = distance * 185;
-            else if (distance > 20 && distance <= 30)
-                total_cost = distance * 170;
-            else if (distance > 30)
-                total_cost = distance * 155;
+            total_cost = 400
+            if(distance > 4){
+                total_cost += (distance - 4) * 16
+            }
         } break
         case 'Tata 407': {
             if (distance > 0 && distance <= 10)
